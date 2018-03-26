3f85423001d9f1dc16a6f81578cb0ce113c0d93d
mnyborg <manyb@dtu.dk>
2018-03-26 14:06:10 +0200

    initiel update based on observable project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+
+
+export class AppComponent implements OnInit {
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get<UserResponse>('https://api.github.com/users/mnyborg').subscribe(data => {
+      console.log('User Login: ' + data.login);
+      console.log('Type: ' + data.type);
+      console.log('Url: ' + data.url);
+    });
+  }
 }
